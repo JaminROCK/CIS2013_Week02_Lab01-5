18799d05b8170ea2bfbe64372daca124447d6226
JaminROCK <KingJamin@hotmail.com>
2018-01-26 01:14:49 -0600

    Update CIS2013_Week02_Lab01.cpp
    tweaks

--- CIS2013_Week02_Lab01.cpp
+++ CIS2013_Week02_Lab01.cpp
@@ -3,31 +3,43 @@ using namespace std;
 
 int main()
 {
+	//Create a room's variables
 	int room_length = 155;
 	int room_width = 155;
 	float room_radius = 15.2;
 	float room_circle_area = (room_radius * room_radius) * 3.14;
 	bool keep_running = true;
-	char question = 'y';
+	char question;
+	//Loop the program
+	while (keep_running)
+	{
 
-	while (keep_running){
+		cout << "Enter your length: ";
+		cin >> room_length;
 
-	cout << "Enter your length: ";
-	cin >> room_length;
+		cout << "Enter your width: ";
+		cin >> room_width;
 
-	cout << "Enter your width: ";
-	cin >> room_width;
-	
-	if(room_length > room_width){
-		cout << "Length is greater than width." << endl;
+		if(room_length > room_width)
+		{
+			cout << "Length is greater than width." << endl;
+		}
+		else 
+		{
+			cout << "Width is greater that length.";
+		}
+
+		/*
+			Multiline comment
+		*/
+		
+		cout << "Run again (y/n): "; //Inline comment
+		cin >> question;
+		//Kill the program
+		if((question !='y') && (question !='Y')) 
+		{
+			keep_running = false;
+		}
 	}
-	else {
-		cout << "Width is greater that length.";}
-
-	cout << "Run again (y/n): ";
-	cin >> question;
-	if((question !='y') && (question !='Y')) {keep_running = false;}
-}
-
 	return 0;
 }
